69d4254fb7bc6811681250087b81ae13d1fbf719
Hafiz Hamza <hafizhamxa8@gmail.com>
2024-01-20 14:08:32 +0500

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -71,7 +71,7 @@ function ResponsiveAppBar() {
               textDecoration: 'none',
             }}
           >
-            LOGO
+          Addx
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
--- src/screens/login/Login.jsx
+++ src/screens/login/Login.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import { TextField, Button, Box, CircularProgress } from '@mui/material';
+import React, { useRef, useState } from 'react';
+
+
 
 export const Login = () => {
+
+ 
+  const [loading, setLoading] = useState(false);
+  const password = useRef()
+  const email = useRef()
+
+  const handleButtonClick = (event) => {
+    event.preventDefault();
+    setLoading(!loading);
+
+    console.log(email.current.value);
+    console.log(password.current.value);
+
+  };
+
   return (
-    <h1>Login</h1>
-  )
-}
+    <Box sx={{ height: '80vh' }} className="d-flex justify-content-center align-item-center">
+      <form  onSubmit={handleButtonClick} className="d-flex justify-content-center flex-column w-25 gap-3">
+        <h1>Login</h1>
+        <TextField id="email" inputRef={email} label="Email" variant="outlined" required />
+        <TextField id="password" inputRef={password} label="Password" variant="outlined" required />
+        <Button type='submit'  variant="contained" disabled={loading} onSubmit={handleButtonClick}>
+          {loading ? <CircularProgress size={20} sx={{ color: 'white' }} /> : 'Login'}
+        </Button>
+      </form>
+    </Box>
+  );
+};
